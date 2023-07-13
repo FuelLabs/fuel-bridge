eab8d9334d3906b291fa4122eccfc0afa1a515db
Luiz Estácio <luizstacio@gmail.com>
2023-07-13 13:23:20 +0200

    chore: add build command

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-echo "\n\Building packages..."
+echo "\n\nBuild projects..."
 pnpm run build
 
 echo "\n\nStarting docker..."
@@ -10,9 +10,12 @@ pnpm run node:up
 
 RUN_TESTS_TRY=0
 MAX_TRIES=50
+OUTPUT=""
 
 runTests() {
     NODE_URL="http://localhost:4000/playground";
+
+    printf "\rWaiting for node.${OUTPUT}"
 
     if [ $RUN_TESTS_TRY -gt $MAX_TRIES ]; then
         echo "\n\nTests failed"
@@ -21,14 +24,14 @@ runTests() {
 
     if curl --silent --head --request GET $NODE_URL | grep "200 OK" > /dev/null; then
         echo "\nRun tests..."
-        # pnpm turbo run test
+        pnpm turbo run test
     else
         # Sleep for 6 seconds before retrying
         sleep 6
         RUN_TESTS_TRY=$((RUN_TESTS_TRY+1))
+        OUTPUT="${OUTPUT}."
         runTests
     fi
 }
 
-echo "\n\nWaiting for node..."
 runTests
