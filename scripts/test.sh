81e0c8b1031a5627e8b67706187c44117e30d7ea
Luiz Estácio <luizstacio@gmail.com>
2023-07-13 02:29:27 +0200

    chore: add timeout on test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,14 +8,24 @@ pnpm run build
 echo "\n\nStarting docker..."
 pnpm run node:up
 
+RUN_TESTS_TRY=0
+MAX_TRIES=50
+
 runTests() {
     NODE_URL="http://localhost:4000/playground";
 
+    if [ $RUN_TESTS_TRY -gt $MAX_TRIES ]; then
+        echo "\n\nTests failed"
+        exit 1
+    fi
+
     if curl --silent --head --request GET $NODE_URL | grep "200 OK" > /dev/null; then
-        echo "\Run tests..."
-        pnpm turbo run test
+        echo "\nRun tests..."
+        # pnpm turbo run test
     else
-        sleep .5
+        # Sleep for 6 seconds before retrying
+        sleep 6
+        RUN_TESTS_TRY=$((RUN_TESTS_TRY+1))
         runTests
     fi
 }
