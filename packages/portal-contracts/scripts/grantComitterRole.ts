3a805ff70ce60637310feac51c2488681b2101c1
DefiCake <rarore92@gmail.com>
2023-08-18 18:54:33 +0200

    feat: add utility to grant comitter role to other addresses

---
+++ packages/portal-contracts/scripts/grantComitterRole.ts
@@ -0,0 +1,43 @@
+import { Signer, ethers } from "ethers";
+import { isAddress } from "ethers/lib/utils";
+import {FuelChainState__factory} from '../typechain';
+import { StaticJsonRpcProvider } from "@ethersproject/providers";
+
+const main = async () => {
+    const {DEPLOYER_KEY, FUEL_CHAIN_STATE_ADDRESS, COMITTER_ADDRESS, RPC_URL} = process.env;
+
+    if(!DEPLOYER_KEY) {
+        throw new Error('No DEPLOYER_KER env var');
+    }
+    
+    if(!RPC_URL) {
+        throw new Error("No RPC_URL env var");
+    }
+
+    if(!FUEL_CHAIN_STATE_ADDRESS || !isAddress(FUEL_CHAIN_STATE_ADDRESS)) {
+        throw new Error(`Invalid env var GRANT_ROLE_TARGET ${FUEL_CHAIN_STATE_ADDRESS}`);
+    } 
+
+    if(!COMITTER_ADDRESS || !isAddress(COMITTER_ADDRESS)) {
+        throw new Error(`Invalid env var COMITTER_ADDRESS ${FUEL_CHAIN_STATE_ADDRESS}`);
+    }
+
+
+    const provider = new StaticJsonRpcProvider(RPC_URL);
+
+    const admin = new ethers.Wallet(DEPLOYER_KEY, provider);
+
+    const target = FuelChainState__factory.connect(FUEL_CHAIN_STATE_ADDRESS, admin);
+    
+    const role = await target.callStatic.COMMITTER_ROLE();
+
+    await target.grantRole(role, COMITTER_ADDRESS).then(tx => {
+        console.log("\t> Transaction sent with hash", tx.hash, "... waiting confirmation");
+        return tx.wait();
+    });
+}
+
+main().then(() => console.log("\t> Succeeded")).catch((err) => {
+    console.error(err);
+    process.exit(1);
+})
