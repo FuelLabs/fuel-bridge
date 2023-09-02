ab412ef1b33dc875ba0e368afc0d15ff8fc37715
DefiCake <rarore92@gmail.com>
2023-09-02 10:44:48 +0200

    chore: run pnpm format

--- packages/portal-contracts/scripts/grantComitterRole.ts
+++ packages/portal-contracts/scripts/grantComitterRole.ts
@@ -1,43 +1,56 @@
-import { Signer, ethers } from "ethers";
-import { isAddress } from "ethers/lib/utils";
-import {FuelChainState__factory} from '../typechain';
-import { StaticJsonRpcProvider } from "@ethersproject/providers";
+import { Signer, ethers } from 'ethers';
+import { isAddress } from 'ethers/lib/utils';
+import { FuelChainState__factory } from '../typechain';
+import { StaticJsonRpcProvider } from '@ethersproject/providers';
 
 const main = async () => {
-    const {DEPLOYER_KEY, FUEL_CHAIN_STATE_ADDRESS, COMITTER_ADDRESS, RPC_URL} = process.env;
+  const { DEPLOYER_KEY, FUEL_CHAIN_STATE_ADDRESS, COMITTER_ADDRESS, RPC_URL } =
+    process.env;
 
-    if(!DEPLOYER_KEY) {
-        throw new Error('No DEPLOYER_KEY env var');
-    }
-    
-    if(!RPC_URL) {
-        throw new Error("No RPC_URL env var");
-    }
+  if (!DEPLOYER_KEY) {
+    throw new Error('No DEPLOYER_KEY env var');
+  }
 
-    if(!FUEL_CHAIN_STATE_ADDRESS || !isAddress(FUEL_CHAIN_STATE_ADDRESS)) {
-        throw new Error(`Invalid env var FUEL_CHAIN_STATE_ADDRESS ${FUEL_CHAIN_STATE_ADDRESS}`);
-    } 
+  if (!RPC_URL) {
+    throw new Error('No RPC_URL env var');
+  }
 
-    if(!COMITTER_ADDRESS || !isAddress(COMITTER_ADDRESS)) {
-        throw new Error(`Invalid env var COMITTER_ADDRESS ${FUEL_CHAIN_STATE_ADDRESS}`);
-    }
+  if (!FUEL_CHAIN_STATE_ADDRESS || !isAddress(FUEL_CHAIN_STATE_ADDRESS)) {
+    throw new Error(
+      `Invalid env var FUEL_CHAIN_STATE_ADDRESS ${FUEL_CHAIN_STATE_ADDRESS}`
+    );
+  }
 
+  if (!COMITTER_ADDRESS || !isAddress(COMITTER_ADDRESS)) {
+    throw new Error(
+      `Invalid env var COMITTER_ADDRESS ${FUEL_CHAIN_STATE_ADDRESS}`
+    );
+  }
 
-    const provider = new StaticJsonRpcProvider(RPC_URL);
+  const provider = new StaticJsonRpcProvider(RPC_URL);
 
-    const admin = new ethers.Wallet(DEPLOYER_KEY, provider);
+  const admin = new ethers.Wallet(DEPLOYER_KEY, provider);
 
-    const target = FuelChainState__factory.connect(FUEL_CHAIN_STATE_ADDRESS, admin);
-    
-    const role = await target.callStatic.COMMITTER_ROLE();
+  const target = FuelChainState__factory.connect(
+    FUEL_CHAIN_STATE_ADDRESS,
+    admin
+  );
 
-    await target.grantRole(role, COMITTER_ADDRESS).then(tx => {
-        console.log("\t> Transaction sent with hash", tx.hash, "... waiting confirmation");
-        return tx.wait();
-    });
-}
+  const role = await target.callStatic.COMMITTER_ROLE();
 
-main().then(() => console.log("\t> Succeeded")).catch((err) => {
+  await target.grantRole(role, COMITTER_ADDRESS).then((tx) => {
+    console.log(
+      '\t> Transaction sent with hash',
+      tx.hash,
+      '... waiting confirmation'
+    );
+    return tx.wait();
+  });
+};
+
+main()
+  .then(() => console.log('\t> Succeeded'))
+  .catch((err) => {
     console.error(err);
     process.exit(1);
-})
+  });
