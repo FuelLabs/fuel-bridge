f280772578cb3e6304fcaf1da16e625ae60659bd
Luiz Estácio <luizstacio@gmail.com>
2023-09-15 22:09:15 -0300

    feat: add configuration to build packages to release to npm

---
+++ packages/portal-contracts/exports/abi.ts
@@ -0,0 +1,2 @@
+export { default as FuelChainState } from '../artifacts/contracts/fuelchain/FuelChainState.sol/FuelChainState.json';
+export { default as FuelMessagePortal } from '../artifacts/contracts/fuelchain/FuelMessagePortal.sol/FuelMessagePortal.json';
