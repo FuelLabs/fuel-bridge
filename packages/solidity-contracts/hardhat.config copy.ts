475964315b53ed5391595a3e5fb227508f8b611a
DefiCake <rarore92@gmail.com>
2023-09-29 19:22:28 +0200

    chore: upgrade hardhat

---
+++ packages/solidity-contracts/hardhat.config copy.ts
@@ -0,0 +1,56 @@
+import { config as dotEnvConfig } from 'dotenv';
+import type { HardhatUserConfig } from 'hardhat/types';
+import '@nomicfoundation/hardhat-chai-matchers';
+import '@nomiclabs/hardhat-etherscan';
+import '@openzeppelin/hardhat-upgrades';
+import 'hardhat-typechain';
+import 'solidity-coverage';
+import 'hardhat-gas-reporter';
+
+dotEnvConfig();
+
+const CONTRACTS_DEPLOYER_KEY = process.env.CONTRACTS_DEPLOYER_KEY || '';
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || '';
+const INFURA_API_KEY = process.env.INFURA_API_KEY || '';
+
+const config: HardhatUserConfig = {
+  defaultNetwork: 'hardhat',
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.9',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 10000,
+          },
+        },
+      },
+    ],
+  },
+  mocha: {
+    timeout: 180_000,
+  },
+  networks: {
+    hardhat: {
+      accounts: {
+        count: 128,
+      },
+    },
+    localhost: {
+      url: 'http://127.0.0.1:8545/',
+    },
+    custom: {
+      url: 'http://127.0.0.1:8545/',
+    },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: CONTRACTS_DEPLOYER_KEY ? [CONTRACTS_DEPLOYER_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+};
+
+export default config;
