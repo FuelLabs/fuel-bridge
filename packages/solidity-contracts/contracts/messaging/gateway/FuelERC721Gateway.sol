9e9e33ba38ce70bc6ab19cd5fdc2d0a480c240ba
DefiCake <rarore92@gmail.com>
2023-10-16 23:30:41 +0200

    chore: update sol contracts to apache 2.0 licnse

--- packages/solidity-contracts/contracts/messaging/gateway/FuelERC721Gateway.sol
+++ packages/solidity-contracts/contracts/messaging/gateway/FuelERC721Gateway.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: Apache-2.0
 pragma solidity 0.8.9;
 
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
