312cc39f68027bbd9ce8163042269f9f19c0ab13
LuizAsFight <felipebolsonigomes@gmail.com>
2023-12-05 18:02:55 -0300

    chore

--- scripts/changeset-next.ts
+++ scripts/changeset-next.ts
@@ -1,4 +1,4 @@
 import { writeFileSync } from 'node:fs';
 
-const output = `---\n"@fuel-bridge/fungible-token": patch\n"@fuel-bridge/integration-tests": patch\n"@fuel-bridge/message-predicates": patch\n"@fuel-bridge/solidity-contracts": patch\n---\n\nincremental\n`;
+const output = `---\n"@fuel-bridge/fungible-token": patch\n"@fuel-bridge/message-predicates": patch\n"@fuel-bridge/solidity-contracts": patch\n---\n\nincremental\n`;
 writeFileSync('.changeset/fuel-labs-ci.md', output);
