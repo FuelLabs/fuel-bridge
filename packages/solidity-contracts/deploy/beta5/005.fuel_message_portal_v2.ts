ef28246a65cc038ff52e8fa582b18be1daac8645
Mad <46090742+DefiCake@users.noreply.github.com>
2023-12-07 18:43:03 +0100

    Deploy scripts with hardhat deploy migrations + OZ upgrades plugin (#107)
    integration between hardhat deploy plugin (features
    migrations, better control over upgrades) and unlocks version control
    for OZ upgrades.

---
+++ packages/solidity-contracts/deploy/beta5/005.fuel_message_portal_v2.ts
@@ -0,0 +1,40 @@
+import type { HardhatRuntimeEnvironment } from 'hardhat/types';
+import type { DeployFunction } from 'hardhat-deploy/dist/types';
+
+import { FuelMessagePortalV2__factory as FuelMessagePortalV2 } from '../../typechain';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const {
+    ethers,
+    upgrades: { upgradeProxy, erc1967 },
+    deployments: { get, save },
+  } = hre;
+  const [deployer] = await ethers.getSigners();
+
+  const fuelMessagePortal = await get('FuelMessagePortal');
+
+  const { deployTransaction, address } = await upgradeProxy(
+    fuelMessagePortal,
+    new FuelMessagePortalV2(deployer),
+    {
+      unsafeAllow: ['constructor'],
+      constructorArgs: [ethers.constants.MaxUint256],
+    }
+  );
+
+  await deployTransaction.wait();
+  const implementation = await erc1967.getImplementationAddress(address);
+
+  console.log('Upgraded FuelMessagePortal at', address);
+  await save('FuelMessagePortal', {
+    address,
+    abi: [],
+    implementation,
+  });
+
+  return true;
+};
+
+func.tags = ['upgrade_portal_v2', 'message_portal_v2', 'FuelMessagePortalV2'];
+func.id = 'fuel_message_portal_v2_upgrade';
+export default func;
