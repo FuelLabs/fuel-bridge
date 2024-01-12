bd66ec0308fd0c2d1c2d89686f6d734f6e4df47a
DefiCake <rarore92@gmail.com>
2024-01-12 16:33:11 +0100

    chore: deploy some testing for beta5

---
+++ packages/solidity-contracts/deploy/beta5/006.erc20_gateway_v1.ts
@@ -0,0 +1,44 @@
+import type { HardhatRuntimeEnvironment } from 'hardhat/types';
+import type { DeployFunction } from 'hardhat-deploy/dist/types';
+
+import { FuelERC20Gateway__factory as FuelERC20GatewayV1 } from '../../typechain';
+
+/**
+ * @description this is deployed just for testing purposes, V1 is a version that
+ * does not validate the correctness of the target fuel contract for deposits
+ */
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const {
+    ethers,
+    upgrades: { deployProxy, erc1967 },
+    deployments: { get, save },
+  } = hre;
+
+  const [deployer] = await ethers.getSigners();
+
+  const fuelMessagePortal = await get('FuelMessagePortal');
+
+  const { deployTransaction, address } = await deployProxy(
+    new FuelERC20GatewayV1(deployer),
+    [fuelMessagePortal.address],
+    {
+      initializer: 'initialize',
+    }
+  );
+
+  await deployTransaction.wait();
+  const implementation = await erc1967.getImplementationAddress(address);
+
+  console.log('Deployed FuelERC20GatewayV1 at', address);
+  await save('FuelERC20GatewayV1', {
+    address,
+    abi: [],
+    implementation,
+  });
+
+  return true;
+};
+
+func.tags = ['erc20V1', 'erc20_gatewayV1', 'FuelERC20GatewayV1'];
+func.id = 'fuel_erc20_gateway_V1';
+export default func;
---
+++ packages/solidity-contracts/deploy/beta5/007.token.ts
@@ -0,0 +1,22 @@
+import type { HardhatRuntimeEnvironment } from 'hardhat/types';
+import type { DeployFunction } from 'hardhat-deploy/dist/types';
+
+/**
+ * @description Deployed just for testing purposes
+ */
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const {
+    ethers,
+    deployments: { deploy },
+  } = hre;
+
+  const [deployer] = await ethers.getSigners();
+
+  await deploy('Token', { from: deployer.address, log: true });
+
+  return true;
+};
+
+func.tags = ['token'];
+func.id = 'token';
+export default func;
