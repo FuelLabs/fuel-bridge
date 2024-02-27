18dee8d2ce9f0609ee791e00ccc2b8ef20454753
LuizAsFight <felipebolsonigomes@gmail.com>
2024-02-27 19:44:13 -0300

    fix: cors

--- packages/solidity-contracts/scripts/serveDeployments.ts
+++ packages/solidity-contracts/scripts/serveDeployments.ts
@@ -15,9 +15,9 @@ import express from 'express';
 const port = process.env.SERVE_PORT || 8080;
 const app: Express = express();
 
-app.use(cors());
-app.use('/', express.static('deployments'));
+app.use('/', cors(), express.static('deployments'));
 
 app.listen(port, () => {
   console.log(`Server is running at https://localhost:${port}`);
+  console.log(`CORS enabled`);
 });
