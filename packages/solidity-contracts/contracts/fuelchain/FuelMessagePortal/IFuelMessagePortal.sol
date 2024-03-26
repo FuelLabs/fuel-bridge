25e907fb6251c663e069198ea0d9d347d14df67d
DefiCake <rarore92@gmail.com>
2024-03-26 18:02:42 +0100

    feat: change TransactionSent event signature

--- packages/solidity-contracts/contracts/fuelchain/FuelMessagePortal/IFuelMessagePortal.sol
+++ packages/solidity-contracts/contracts/fuelchain/FuelMessagePortal/IFuelMessagePortal.sol
@@ -6,9 +6,9 @@ import {FuelMessagePortalV3} from "./v3/FuelMessagePortalV3.sol";
 /// @notice to be used by external tools, like the block producer
 /// @dev marked abstract to track all relevant current and future functions as development evolves
 abstract contract IFuelMessagePortal is FuelMessagePortalV3 {
-    event TransactionSent(bytes serializedTx, uint64);
+    event Transaction(uint64 max_gas, bytes canonically_serialized_tx);
 
-    function sendTransaction(bytes calldata serializedTx, uint64 gas) external payable virtual;
+    function sendTransaction(uint64 gas, bytes calldata serializedTx) external payable virtual;
 
     function getLastSeenBlock() external virtual returns (uint256);
 
