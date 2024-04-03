69d40396a6a847da888fc04461eadb6c65022c2b
Mad <46090742+DefiCake@users.noreply.github.com>
2024-04-03 14:35:58 +0200

    Add nonce to FTI interface (#155)
    Closes #154

--- packages/solidity-contracts/contracts/fuelchain/FuelMessagePortal/IFuelMessagePortal.sol
+++ packages/solidity-contracts/contracts/fuelchain/FuelMessagePortal/IFuelMessagePortal.sol
@@ -6,7 +6,7 @@ import {FuelMessagePortalV3} from "./v3/FuelMessagePortalV3.sol";
 /// @notice to be used by external tools, like the block producer
 /// @dev marked abstract to track all relevant current and future functions as development evolves
 abstract contract IFuelMessagePortal is FuelMessagePortalV3 {
-    event Transaction(uint64 max_gas, bytes canonically_serialized_tx);
+    event Transaction(uint256 indexed nonce, uint64 max_gas, bytes canonically_serialized_tx);
 
     function sendTransaction(uint64 gas, bytes calldata serializedTx) external payable virtual;
 
