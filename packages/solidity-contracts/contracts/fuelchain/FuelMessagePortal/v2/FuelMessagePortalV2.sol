facad903f652b3eadae96744824548dff76fc88c
DefiCake <rarore92@gmail.com>
2024-04-17 19:07:25 +0200

    refactor: update comment on FuelMessagePortalV2

--- packages/solidity-contracts/contracts/fuelchain/FuelMessagePortal/v2/FuelMessagePortalV2.sol
+++ packages/solidity-contracts/contracts/fuelchain/FuelMessagePortal/v2/FuelMessagePortalV2.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.9;
 import "../../FuelMessagePortal.sol";
 
 /// @custom:oz-upgrades-unsafe-allow constructor state-variable-immutable
-/// @custom:deprecation This contract is superseded by FuelMessagePortalV3
+/// @custom:deprecation THIS CONTRACT IS DEPRECATED. CHECK FuelMessagePortalV3
 contract FuelMessagePortalV2 is FuelMessagePortal {
     error GlobalDepositLimit();
 
