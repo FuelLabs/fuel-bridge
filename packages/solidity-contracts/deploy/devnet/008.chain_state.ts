fcd21647c1807a41b95a34c62ae7bf392b7bf2d5
DefiCake <rarore92@gmail.com>
2024-05-07 20:38:11 +0200

    chore: upgrade FuelChainState

--- packages/solidity-contracts/deploy/devnet/008.chain_state.ts
+++ packages/solidity-contracts/deploy/devnet/008.chain_state.ts
@@ -1,44 +1,46 @@
-import { MaxUint256 } from 'ethers';
 import type { HardhatRuntimeEnvironment } from 'hardhat/types';
 import type { DeployFunction } from 'hardhat-deploy/dist/types';
 
-import { FuelMessagePortalV3__factory as FuelMessagePortal } from '../../typechain';
+import { FuelChainState__factory as FuelChainState } from '../../typechain';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {
     ethers,
     upgrades: { upgradeProxy, erc1967 },
-    deployments: { get, save },
+    deployments: { save, execute, get },
   } = hre;
   const [deployer] = await ethers.getSigners();
 
-  const { address: fuelMessagePortalAddress } = await get('FuelMessagePortal');
+  const { address: fuelChainStateAddress } = await get('FuelChainState');
 
+  console.log('Upgrading FuelChainState...');
   const contract = await upgradeProxy(
-    fuelMessagePortalAddress,
-    new FuelMessagePortal(deployer),
-    {
-      constructorArgs: [MaxUint256],
-    }
+    fuelChainStateAddress,
+    new FuelChainState(deployer)
   );
   await contract.waitForDeployment();
-
   const tx = contract.deploymentTransaction();
   await tx.wait();
 
   const address = await contract.getAddress();
   const implementation = await erc1967.getImplementationAddress(address);
 
-  console.log('Deployed FuelMessagePortal at', address);
-  await save('FuelMessagePortal', {
+  console.log('Deployed new implementation at', implementation);
+  await save('FuelChainState', {
     address,
-    abi: [...FuelMessagePortal.abi],
+    abi: [...FuelChainState.abi],
     implementation,
   });
+
+  await execute(
+    'FuelChainState',
+    { log: true, from: deployer.address },
+    'unpause'
+  );
 
   return true;
 };
 
-func.tags = ['portal_redeploy'];
-func.id = 'fuel_message_portal_redeploy';
+func.tags = ['state_redeploy'];
+func.id = 'state_redeploy';
 export default func;
