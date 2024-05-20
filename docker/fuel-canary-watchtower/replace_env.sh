96200969cd26e09d2cae99e69de292a80cbbd475
Salka1988 <emirsalkicart@gmail.com>
2024-05-20 14:33:43 +0200

    fix docker

--- docker/fuel-canary-watchtower/replace_env.sh
+++ docker/fuel-canary-watchtower/replace_env.sh
@@ -1,65 +1,10 @@
 #!/bin/sh
-set -euo
 
-RETRIES=${RETRIES:-90}
-JSON='{"jsonrpc":"2.0","id":0,"method":"net_version","params":[]}'
-FUEL_DB_PATH=./mnt/db/
+CONFIG_FILE=$1
 
-if [ -z "$L1_CHAIN_HTTP" ]; then
-    echo "Must specify \$L1_CHAIN_HTTP."
-    exit 1
-fi
-if [ -z "$DEPLOYMENTS_HTTP" ]; then
-    echo "Must specify \$DEPLOYMENTS_HTTP."
-    exit 1
-fi
+# Replace placeholders with environment variables
+sed -i "s|{{FUEL_GRAPHQL_ENDPOINT}}|${FUEL_GRAPHQL_ENDPOINT}|g" $CONFIG_FILE
+sed -i "s|{{ETHEREUM_RPC}}|${ETHEREUM_RPC}|g" $CONFIG_FILE
 
-# wait for the base layer to be up
-echo "Waiting for l1 chain."
-curl \
-    --fail \
-    --show-error \
-    --silent \
-    -H "Content-Type: application/json" \
-    --retry-connrefused \
-    --retry $RETRIES \
-    --retry-delay 1 \
-    -d $JSON \
-    $L1_CHAIN_HTTP > /dev/null
-echo "Connected to l1 chain."
-
-# get the deployments file from the deployer
-echo "Waiting for l1 chain deployment data."
-curl \
-    --fail \
-    --show-error \
-    --silent \
-    --retry-connrefused \
-    --retry-all-errors \
-    --retry $RETRIES \
-    --retry-delay 5 \
-    $DEPLOYMENTS_HTTP \
-    -o addresses.json
-echo "Got l1 chain deployment data."
-
-# pull data from deployer dump
-export FUEL_MESSAGE_PORTAL_CONTRACT_ADDRESS=$(cat "./addresses.json" | jq -r .FuelMessagePortal)
-echo "FUEL_MESSAGE_PORTAL_CONTRACT_ADDRESS: $FUEL_MESSAGE_PORTAL_CONTRACT_ADDRESS"
-echo "L1_CHAIN_HTTP: $L1_CHAIN_HTTP"
-
-# start the Fuel client
-#--db-path ${FUEL_DB_PATH}
-echo "Starting fuel node."
-exec /root/fuel-core run \
-    --ip $FUEL_IP \
-    --port $FUEL_PORT \
-    --db-type in-memory \
-    --utxo-validation \
-    --vm-backtrace \
-    --enable-relayer \
-    --relayer $L1_CHAIN_HTTP \
-    --relayer-v2-listening-contracts $FUEL_MESSAGE_PORTAL_CONTRACT_ADDRESS \
-    --poa-interval-period 1sec \
-    --debug \
-    --min-gas-price 0 \
-    --snapshot ./
+# Execute the main application
+/app/fuel-canary-watchtower $CONFIG_FILE
