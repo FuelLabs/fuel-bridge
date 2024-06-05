8bdd1c68b891d80e9110db23f37e127ba36a8e2b
DefiCake <rarore92@gmail.com>
2024-06-05 11:31:09 +0200

    feat: add withdrawal pausing scripts

--- packages/solidity-contracts/scripts/hardhat/index.ts
+++ packages/solidity-contracts/scripts/hardhat/index.ts
@@ -2,3 +2,5 @@ export * from './pause';
 export * from './unpause';
 export * from './grantRole';
 export * from './depositETH';
+export * from './withdrawalPause';
+export * from './withdrawalResume';
