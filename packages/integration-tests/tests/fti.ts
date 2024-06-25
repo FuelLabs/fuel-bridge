4701e1b9f20ad228f57246592b3aa01189b9590e
DefiCake <rarore92@gmail.com>
2024-06-25 19:08:29 +0200

    test: complete fti integration tests

--- packages/integration-tests/tests/fti.ts
+++ packages/integration-tests/tests/fti.ts
@@ -9,7 +9,7 @@ const { expect } = chai;
 
 const MAX_GAS = 10000000n;
 
-describe.only('Forced Transaction Inclusion', async function () {
+describe('Forced Transaction Inclusion', async function () {
   // Timeout 6 minutes
   const DEFAULT_TIMEOUT_MS: number = 400_000;
   let BASE_ASSET_ID: string;
@@ -41,20 +41,6 @@ describe.only('Forced Transaction Inclusion', async function () {
       fuelSenderBalance = await fuelSender.getBalance(BASE_ASSET_ID);
       fuelReceiverBalance = await fuelReceiver.getBalance(BASE_ASSET_ID);
     });
-
-    // it.skip('deposit to wallet address', async () => {
-    //   await env.eth.fuelMessagePortal.depositETH(fuelSender.address.toB256(), {
-    //     value: parseEther('1'),
-    //   });
-
-    //   let bal = await fuelSender.getBalance(BASE_ASSET_ID);
-
-    //   while (bal.toString() === '0') {
-    //     bal = await fuelSender.getBalance(BASE_ASSET_ID);
-    //   }
-
-    //   fuelSenderBalance = await fuelSender.getBalance(BASE_ASSET_ID);
-    // });
 
     it('allows to send transactions', async () => {
       const transferRequest = await fuelSender.createTransfer(
@@ -120,8 +106,120 @@ describe.only('Forced Transaction Inclusion', async function () {
       expect(txResult.status).to.equal('success');
     });
 
-    it('rejects transactions without signatures');
+    it('rejects transactions without signatures', async () => {
+      const transferRequest = await fuelSender.createTransfer(
+        fuelReceiver.address,
+        fuelSenderBalance.div(10),
+        BASE_ASSET_ID
+      );
 
-    it('rejects transactions without gas');
+      const transactionRequest = transactionRequestify(transferRequest);
+      await env.fuel.provider.estimateTxDependencies(transactionRequest);
+
+      const fuelSerializedTx = hexlify(transactionRequest.toTransactionBytes());
+
+      const gasPrice = await env.eth.fuelMessagePortal.MIN_GAS_PRICE();
+      const expectedFee = gasPrice * MAX_GAS;
+
+      const ethTx = await env.eth.fuelMessagePortal.sendTransaction(
+        MAX_GAS,
+        fuelSerializedTx,
+        { value: expectedFee }
+      );
+
+      const { blockNumber } = await ethTx.wait();
+
+      const [event] = await env.eth.fuelMessagePortal.queryFilter(
+        env.eth.fuelMessagePortal.filters.Transaction,
+        blockNumber,
+        blockNumber
+      );
+
+      expect(event.args.canonically_serialized_tx).to.be.equal(
+        fuelSerializedTx
+      );
+
+      const payload = solidityPacked(
+        ['uint256', 'uint64', 'bytes'],
+        [
+          event.args.nonce,
+          event.args.max_gas,
+          event.args.canonically_serialized_tx,
+        ]
+      );
+      const relayedTxId = sha256(payload);
+      const fuelTxId = transactionRequest.getTransactionId(CHAIN_ID);
+
+      const { response, error } = await waitForTransaction(
+        fuelTxId,
+        env.fuel.provider,
+        {
+          relayedTxId,
+        }
+      );
+
+      expect(response).to.be.null;
+      expect(error).to.contain('InvalidSignature');
+    });
+
+    it('rejects transactions without enough gas', async () => {
+      const transferRequest = await fuelSender.createTransfer(
+        fuelReceiver.address,
+        fuelSenderBalance.div(10),
+        BASE_ASSET_ID
+      );
+
+      const transactionRequest = transactionRequestify(transferRequest);
+      await env.fuel.provider.estimateTxDependencies(transactionRequest);
+
+      const signature = await fuelSender.signTransaction(transactionRequest);
+      transactionRequest.updateWitnessByOwner(fuelSender.address, signature);
+
+      const fuelSerializedTx = hexlify(transactionRequest.toTransactionBytes());
+
+      const gasPrice = await env.eth.fuelMessagePortal.MIN_GAS_PRICE();
+      const minGasPerTx = await env.eth.fuelMessagePortal.MIN_GAS_PER_TX();
+      const expectedFee = gasPrice * minGasPerTx;
+
+      const ethTx = await env.eth.fuelMessagePortal.sendTransaction(
+        minGasPerTx,
+        fuelSerializedTx,
+        { value: expectedFee }
+      );
+
+      const { blockNumber } = await ethTx.wait();
+
+      const [event] = await env.eth.fuelMessagePortal.queryFilter(
+        env.eth.fuelMessagePortal.filters.Transaction,
+        blockNumber,
+        blockNumber
+      );
+
+      expect(event.args.canonically_serialized_tx).to.be.equal(
+        fuelSerializedTx
+      );
+
+      const payload = solidityPacked(
+        ['uint256', 'uint64', 'bytes'],
+        [
+          event.args.nonce,
+          event.args.max_gas,
+          event.args.canonically_serialized_tx,
+        ]
+      );
+      const relayedTxId = sha256(payload);
+      const fuelTxId = transactionRequest.getTransactionId(CHAIN_ID);
+
+      const { response, error } = await waitForTransaction(
+        fuelTxId,
+        env.fuel.provider,
+        {
+          relayedTxId,
+        }
+      );
+
+      expect(response).to.be.null;
+      expect(error).to.contain('Insufficient');
+    });
   });
 });
