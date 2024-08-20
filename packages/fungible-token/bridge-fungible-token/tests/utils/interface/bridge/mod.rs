2d140c49ffb8e7023b927d6caf4b047b0e85be8c
DefiCake <rarore92@gmail.com>
2024-08-20 12:21:01 +0200

    chore: replace FuelCallResponse with CallResponse

--- packages/fungible-token/bridge-fungible-token/tests/utils/interface/bridge/mod.rs
+++ packages/fungible-token/bridge-fungible-token/tests/utils/interface/bridge/mod.rs
@@ -5,7 +5,7 @@ use crate::utils::{
 use fuels::{
     accounts::wallet::WalletUnlocked,
     prelude::{CallParameters, TxPolicies},
-    programs::call_response::FuelCallResponse,
+    programs::responses::CallResponse,
     types::{bech32::Bech32ContractId, Bits256},
 };
 
@@ -15,7 +15,7 @@ pub(crate) async fn claim_refund(
     originator: Bits256,
     token_address: Bits256,
     token_id: Bits256,
-) -> FuelCallResponse<()> {
+) -> CallResponse<()> {
     contract
         .methods()
         .claim_refund(originator, token_address, token_id)
@@ -31,7 +31,7 @@ pub(crate) async fn withdraw(
     to: Bits256,
     amount: u64,
     gas: u64,
-) -> FuelCallResponse<()> {
+) -> CallResponse<()> {
     let tx_policies = TxPolicies::new(Some(0), None, Some(0), None, Some(gas));
     let contract_id = contract.contract_id();
     let asset_id = get_asset_id(contract_id, BRIDGED_TOKEN);
