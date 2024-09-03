6c34d491aea0fc8a9c90562712deffa669f39020
DefiCake <rarore92@gmail.com>
2024-09-03 19:24:51 +0200

    feat: add L2 bridge proxy owner transfer script

---
+++ packages/test-utils/src/scripts/transfer-bridge-ownership.ts
@@ -0,0 +1,94 @@
+/**
+ * This is a stand-alone script that upgrades the bridge
+ */
+
+import {
+  Proxy,
+  BridgeFungibleTokenFactory,
+  BridgeFungibleToken,
+} from '@fuel-bridge/fungible-token';
+
+import {
+  DeployContractResult,
+  Provider,
+  Wallet,
+  WalletUnlocked,
+  ZeroBytes32,
+} from 'fuels';
+import { debug } from '../utils';
+
+const { L1_TOKEN_GATEWAY, L2_SIGNER, L2_RPC, L2_BRIDGE_ID, L2_NEW_OWNER } =
+  process.env;
+
+// This helper avoids an exception in the case that the contract
+// was already deployed, and returns the contract instead
+function fetchIfDeployed(provider: Provider, wallet: WalletUnlocked) {
+  return async (tx: DeployContractResult) => {
+    const contract = await provider.getContract(tx.contractId);
+
+    if (!contract) return tx.waitForResult();
+    else {
+      await tx.waitForResult().catch(() => {});
+      return {
+        contract: new BridgeFungibleToken(contract.id, wallet),
+      };
+    }
+  };
+}
+
+const main = async () => {
+  const provider = await Provider.create(L2_RPC, { resourceCacheTTL: -1 });
+  const wallet = Wallet.fromPrivateKey(L2_SIGNER, provider);
+
+  const proxy = new Proxy(L2_BRIDGE_ID, wallet);
+
+  console.log('\t> L2 Bridge deployment script initiated');
+  console.log('\t> Loaded wallet', wallet.address.toB256());
+  console.log('\t> Balance: ', (await wallet.getBalance()).toString());
+
+  debug('Detecting if the bridge is a proxy...');
+  let owner: string | null = await proxy.functions
+    ._proxy_owner()
+    .dryRun()
+    .then((result) => {
+      debug('bridge._proxy.owner() succeeded, assuming proxy');
+      return result.value.Initialized.Address.bits;
+    })
+    .catch((e) => {
+      debug(`bridge._proxy_owner() failed with error: `);
+      debug(`${JSON.stringify(e, undefined, 2)}`);
+      return null;
+    });
+
+  if (owner === null) {
+    console.log('Could not fetch the bridge owner, is it a proxy?');
+    return;
+  }
+
+  if (
+    owner.replace('0x', '').toLowerCase() !==
+    wallet.address.toB256().replace('0x', '').toLowerCase()
+  ) {
+    console.log(`Owner mismatch, contract owned by ${owner}`);
+    return;
+  }
+
+  const addressInput = { bits: L2_NEW_OWNER };
+  const addressIdentityInput = { Address: addressInput };
+  const tx = await proxy.functions
+    ._proxy_change_owner(addressIdentityInput)
+    .call();
+
+  console.log('\tTransaction ID: ', tx.transactionId);
+  await tx.waitForResult();
+};
+
+main()
+  .then(() => {
+    console.log('\t> Finished');
+    process.exit(0);
+  })
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
