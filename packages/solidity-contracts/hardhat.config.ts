69f993ef2b6510f8dd726970fbfd1cea7f4f67c3
viraj124 <virajm72@gmail.com>
2024-09-25 23:15:45 +0530

    chore: revert hardhat config updates

--- packages/solidity-contracts/hardhat.config.ts
+++ packages/solidity-contracts/hardhat.config.ts
@@ -38,9 +38,6 @@ const config: HardhatUserConfig = {
   networks: {
     hardhat: {
       accounts: {
-        accountsBalance: "10000000000000000000000",
-        mnemonic:
-        'test test test test test test test test test test test junk',
         count: 128,
       },
       deploy: ['deploy/hardhat'],
