307daeb5f5b9b4063f1e7a0592a655f7222d23ec
viraj124 <virajm72@gmail.com>
2024-10-03 16:44:10 +0530

    refactor: update solidity compiler to 0.8.24

--- packages/solidity-contracts/hardhat.config.ts
+++ packages/solidity-contracts/hardhat.config.ts
@@ -22,8 +22,9 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: '0.8.9',
+        version: '0.8.24',
         settings: {
+          evmVersion: 'cancun',
           optimizer: {
             enabled: true,
             runs: 10000,
