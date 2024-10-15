637bee6c1361e438322148a38d2eba2273c59d3c
viraj124 <virajm72@gmail.com>
2024-10-15 12:40:43 +0530

    feat: add grant role event filter script

---
+++ packages/solidity-contracts/scripts/hardhat/eventFilter.ts
@@ -0,0 +1,45 @@
+import { task } from 'hardhat/config';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { writeFileSync } from 'fs';
+
+task(
+  'grant-role-event-filter',
+  'Filters grant role event for a specific contract to keep track of assigned roles'
+)
+  .addParam('contract', 'address of the contract')
+  .setAction(
+    async (taskArgs: any, hre: HardhatRuntimeEnvironment): Promise<void> => {
+      const provider = new hre.ethers.JsonRpcProvider(process.env.RPC_URL);
+
+      const grantRoleEvent = [
+        'event RoleGranted(bytes32 indexed role, address indexed account, address indexed sender)',
+      ];
+
+      const contract = new hre.ethers.Contract(
+        taskArgs.contract,
+        grantRoleEvent,
+        provider
+      );
+
+      const eventPayload: any = [];
+
+      try {
+        const events = await contract.queryFilter(
+          contract.filters.RoleGranted()
+        );
+
+        events.forEach((event) => {
+          const eventArgs: any = {};
+          eventArgs.role = event.args[0];
+          eventArgs.account = event.args[1];
+          eventArgs.sender = event.args[2];
+
+          eventPayload.push(eventArgs);
+        });
+
+        writeFileSync('grantedRoles.json', JSON.stringify(eventPayload));
+      } catch (error) {
+        throw new Error(`Unable to query events: ${error}`);
+      }
+    }
+  );
