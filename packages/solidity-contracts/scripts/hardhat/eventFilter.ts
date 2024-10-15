6dfd2d4f2d70fb0a56ed7c80809e7902c73c3b35
viraj124 <virajm72@gmail.com>
2024-10-15 15:41:35 +0530

    refactor: add check for active roles

--- packages/solidity-contracts/scripts/hardhat/eventFilter.ts
+++ packages/solidity-contracts/scripts/hardhat/eventFilter.ts
@@ -12,7 +12,74 @@ task(
       const provider = new hre.ethers.JsonRpcProvider(process.env.RPC_URL);
 
       const grantRoleEvenABI = [
-        'event RoleGranted(bytes32 indexed role, address indexed account, address indexed sender)',
+        {
+          inputs: [
+            {
+              internalType: 'bytes32',
+              name: 'role',
+              type: 'bytes32',
+            },
+            {
+              internalType: 'address',
+              name: 'account',
+              type: 'address',
+            },
+          ],
+          name: 'hasRole',
+          outputs: [
+            {
+              internalType: 'bool',
+              name: '',
+              type: 'bool',
+            },
+          ],
+          stateMutability: 'view',
+          type: 'function',
+        },
+        {
+          anonymous: false,
+          inputs: [
+            {
+              indexed: true,
+              internalType: 'bytes32',
+              name: 'role',
+              type: 'bytes32',
+            },
+            {
+              indexed: true,
+              internalType: 'address',
+              name: 'account',
+              type: 'address',
+            },
+            {
+              indexed: true,
+              internalType: 'address',
+              name: 'sender',
+              type: 'address',
+            },
+          ],
+          name: 'RoleGranted',
+          type: 'event',
+        },
+      ];
+
+      // existing roles
+      const DEFAULT_ADMIN_ROLE = hre.ethers.ZeroHash;
+      const PAUSER_ROLE = hre.ethers.keccak256(
+        hre.ethers.toUtf8Bytes('PAUSER_ROLE')
+      );
+      const COMMITTER_ROLE = hre.ethers.keccak256(
+        hre.ethers.toUtf8Bytes('COMMITTER_ROLE')
+      );
+      const SET_RATE_LIMITER_ROLE = hre.ethers.keccak256(
+        hre.ethers.toUtf8Bytes('SET_RATE_LIMITER_ROLE')
+      );
+
+      const roles = [
+        { name: 'DEFAULT_ADMIN_ROLE', value: DEFAULT_ADMIN_ROLE },
+        { name: 'PAUSER_ROLE', value: PAUSER_ROLE },
+        { name: 'COMMITTER_ROLE', value: COMMITTER_ROLE },
+        { name: 'SET_RATE_LIMITER_ROLE', value: SET_RATE_LIMITER_ROLE },
       ];
 
       const contract = new hre.ethers.Contract(
@@ -28,15 +95,20 @@ task(
           contract.filters.RoleGranted()
         );
 
-        events.forEach((event: any) => {
-          // Typing bug in the `event` type
-          const eventArgs: any = {};
-          eventArgs.role = event.args[0];
-          eventArgs.account = event.args[1];
-          eventArgs.sender = event.args[2];
+        for (const event of events) {
+          // only checking for active roles
+          const hasRole = await contract.hasRole(event.args[0], event.args[1]);
+          if (hasRole) {
+            const eventArgs: any = {};
+            // computing the `role` in a readable format
+            eventArgs.role =
+              roles.find((role) => role.value === event.args[0])?.name ||
+              'UNKNOWN_ROLE';
+            eventArgs.account = event.args[1];
 
-          eventPayload.push(eventArgs);
-        });
+            eventPayload.push(eventArgs);
+          }
+        }
 
         writeFileSync('grantedRoles.json', JSON.stringify(eventPayload));
       } catch (error) {
