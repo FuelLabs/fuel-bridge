f77346cf445e10ad41fde7a6130ae3893f700b65
Viraz Malhotra <virajm72@gmail.com>
2024-10-22 10:03:04 +0530

    feat: add support for permit based tokens in erc20 gateway (#329)
    - closes #156 
    - implements [EIP 2612](https://eips.ethereum.org/EIPS/eip-2612) in the
    gateway

---
+++ packages/solidity-contracts/contracts/utils/Permit.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity 0.8.9;
+
+/// @notice Structure for erc20 token permit signature
+struct PermitSignature {
+    uint256 deadline;
+    uint8 v;
+    bytes32 r;
+    bytes32 s;
+}
