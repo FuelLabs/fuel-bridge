362d2460f46fe321a2fd6e07322b2da4fd6b37d7
Mad <46090742+DefiCake@users.noreply.github.com>
2024-11-26 15:24:33 +0100

    chore: remove ETH caps (#362)

---
+++ packages/solidity-contracts/deploy/mainnet/020.portal_upgrade.ts
@@ -0,0 +1,53 @@
+import { password } from '@inquirer/prompts';
+import type { TransactionResponse } from 'ethers';
+import { MaxUint256 } from 'ethers';
+import type { HardhatRuntimeEnvironment } from 'hardhat/types';
+import type { DeployFunction } from 'hardhat-deploy/dist/types';
+
+import { FuelMessagePortalV3__factory as FuelMessagePortalV3 } from '../../typechain';
+
+const RATE_LIMIT_DURATION = 3600 * 24 * 7;
+
+const ETH_DEPOSIT_CAP = MaxUint256;
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const {
+    ethers,
+    upgrades: { prepareUpgrade },
+    deployments: { get, save },
+  } = hre;
+
+  const privateKey = await password({ message: 'Enter private key' });
+  const deployer = new ethers.Wallet(privateKey, ethers.provider);
+
+  const { address } = await get('FuelMessagePortal');
+
+  const constructorArgs = [ETH_DEPOSIT_CAP.toString(), RATE_LIMIT_DURATION];
+
+  const tx = (await prepareUpgrade(address, new FuelMessagePortalV3(deployer), {
+    constructorArgs,
+    getTxResponse: true,
+  })) as TransactionResponse;
+  const receipt = await tx.wait();
+
+  const implementation = receipt?.contractAddress;
+
+  if (!implementation) {
+    throw new Error('No contract in receipt');
+  }
+
+  console.log('Proposed FuelMessagePortal upgrade to', implementation);
+  await save('FuelMessagePortal', {
+    address,
+    abi: [...FuelMessagePortalV3.abi],
+    implementation,
+    transactionHash: tx.hash,
+    linkedData: { factory: 'FuelMessagePortalV3', constructorArgs },
+  });
+
+  return true;
+};
+
+func.tags = ['20_upgrade_portal'];
+func.id = '20_upgrade_portal';
+export default func;
