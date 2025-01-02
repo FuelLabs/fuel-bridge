2ada6f56864baa9965299b7fa805218fc95a3ed8
viraj124 <virajm72@gmail.com>
2025-01-02 15:45:42 +0530

    chore: update order of commands in check script

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-pnpm forc fmt --check
 cargo fmt --check
 pnpm forc build --release
+pnpm forc fmt --check
 cargo clippy --all-features --all-targets -- -D warnings
 pnpm prettier:check
 pnpm lint:check
