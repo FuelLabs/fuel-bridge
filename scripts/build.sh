38ba169230bbc2fbc24fe3799fff8cced13975bb
viraj124 <virajm72@gmail.com>
2025-01-03 13:04:59 +0530

    chore: update fmt command

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 forc build --release
+pnpm forc fmt --path packages/message-predicates --check
 cargo run --bin fuel-contract-message-predicate
 turbo run build
-pnpm forc fmt --check
