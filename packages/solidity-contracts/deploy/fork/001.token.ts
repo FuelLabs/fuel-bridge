b384ca55f067742ea40dd0a4c038e5887d348bbc
viraj124 <virajm72@gmail.com>
2025-01-08 16:04:27 +0530

    chore: relocated upgrade scripts to a different deploy folder

---
+++ packages/solidity-contracts/deploy/fork/001.token.ts
@@ -0,0 +1,20 @@
+import type { HardhatRuntimeEnvironment } from 'hardhat/types';
+import type { DeployFunction } from 'hardhat-deploy/dist/types';
+
+/**
+ * @description Deployed for testing purposes
+ */
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const {
+    ethers,
+    deployments: { deploy },
+  } = hre;
+
+  const [deployer] = await ethers.getSigners();
+
+  await deploy('Token', { from: deployer.address, log: true });
+};
+
+func.tags = ['token'];
+func.id = 'token';
+export default func;
---
+++ packages/solidity-contracts/deploy/fork/999.serve_deployment_file.ts
@@ -0,0 +1,80 @@
+import fs, { writeFile } from 'fs';
+import type { HardhatRuntimeEnvironment } from 'hardhat/types';
+import type { DeployFunction } from 'hardhat-deploy/dist/types';
+import path from 'path';
+import { promisify } from 'util';
+
+import { FuelChainState__factory } from '../../typechain';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, ethers } = hre;
+
+  let address: any;
+  const isForking = hre.config.networks[hre.network.name]?.forking?.enabled;
+  const deploymentsFile: { [name: string]: string } = {};
+
+  if (isForking) {
+    let deploymentDir = path.join(
+      __dirname,
+      '..',
+      '..',
+      '/',
+      'deployments',
+      'mainnet'
+    );
+
+    fs.readdirSync(deploymentDir)
+      .filter((file) => path.extname(file) === '.json')
+      .forEach((file) => {
+        const filePath = path.join(deploymentDir, file);
+        try {
+          const deployment = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+          // Use filename (without .json) as the key
+          const contractName = path.basename(file, '.json');
+          deploymentsFile[contractName] = deployment.address;
+        } catch (error) {
+          console.error(`Error reading deployment file ${file}:`, error);
+        }
+      });
+
+    deploymentDir = path.join(
+      __dirname,
+      '..',
+      '..',
+      '/',
+      'deployments',
+      'mainnet',
+      'FuelChainState.json'
+    );
+
+    const chainStateDeployment = JSON.parse(
+      fs.readFileSync(deploymentDir, 'utf8')
+    );
+    address = chainStateDeployment.address;
+  }
+
+  const state = FuelChainState__factory.connect(address, ethers.provider);
+
+  deploymentsFile['BLOCKS_PER_COMMIT_INTERVAL'] = (
+    await state.BLOCKS_PER_COMMIT_INTERVAL()
+  ).toString();
+  deploymentsFile['NUM_COMMIT_SLOTS'] = (
+    await state.NUM_COMMIT_SLOTS()
+  ).toString();
+  deploymentsFile['TIME_TO_FINALIZE'] = (
+    await state.TIME_TO_FINALIZE()
+  ).toString();
+
+  const writeFileAsync = promisify(writeFile);
+
+  await writeFileAsync(
+    'deployments/deployments.local.json',
+    JSON.stringify(deploymentsFile, null, 2),
+    'utf8'
+  );
+};
+
+func.tags = ['all'];
+func.id = 'all';
+func.runAtTheEnd = true;
+export default func;
